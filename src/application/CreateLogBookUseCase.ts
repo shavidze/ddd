2b88bd83aed1a26635a18d4fd743884f41e0979d
Saba Shavidze <sabashavidze@gmail.com>
2024-04-09 14:10:39 +0400

    Initialize

---
+++ src/application/CreateLogBookUseCase.ts
@@ -0,0 +1,36 @@
+import { randomUUID } from "crypto"
+
+interface ICreateLogbookDto {
+  userId: string
+  name: string
+}
+
+interface ICreateLogbookResult {
+  logbookId: string
+}
+
+class LogBook {
+  public constructor(
+    public readonly name: string,
+    public readonly userId: string = randomUUID()
+  ) {}
+}
+
+// for DB use dependency inversion principle
+
+interface ILogbookRepository {
+  save(logbook: LogBook): Promise<boolean>
+}
+
+export class CreateLogBookUseCase {
+  public constructor(private readonly _logbookRepo: ILogbookRepository) {}
+
+  public async execute(
+    input: ICreateLogbookDto
+  ): Promise<ICreateLogbookResult> {
+    const logbook = new LogBook(input.name, input.userId)
+    return {
+      logbookId: "",
+    }
+  }
+}
